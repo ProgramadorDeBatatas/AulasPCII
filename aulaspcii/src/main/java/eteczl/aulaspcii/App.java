45097b0ab74898a41b47ad49025bd6ed2b1dbc71
wagnermarques <wagnerdocri@gmail.com>
2017-07-21 02:29:31 -0300

    adicionei um proj maven

---
+++ aulaspcii/src/main/java/eteczl/aulaspcii/App.java
@@ -0,0 +1,13 @@
+package eteczl.aulaspcii;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
